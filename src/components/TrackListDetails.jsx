9253e73f9ffdacb5ea08c280adc40314843dc745
Oksana <myzove@mail.ru>
2020-12-02 04:21:32 +0200

    adding description to descr component

--- src/components/TrackListDetails.jsx
+++ src/components/TrackListDetails.jsx
@@ -6,7 +6,13 @@ const TrackListDetails = () => {
     console.log('props', this.props.match.params.mbid)
     return (
         <Container>
-
+            <Row>
+                <Col>
+                    <h4>Singer name: </h4>
+                    {/* <img src=''/> */}
+                    <h4>Description </h4>
+                </Col>
+            </Row>
         </Container>
         
     )
